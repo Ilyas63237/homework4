12d29944a180dc9651cecdda3ee2b953da0cfc85
Ilyas63237 <vakhitov.ilyas@bk.ru>
2024-07-12 10:56:29 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('Кем вы работаете? ')
+print(len(my_string))
+print('Я тоже работаю ', my_string)
+print("СТОРОКА В НИЖНЕМ РЕГИСТРЕ".lower())
+print("сторока в нижнем регистре".upper())
+print('Кем вы работаете? '.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
